d5689c1cdbfc4e07982e628e8a709699dfd88191
RositcaMadzharova <val.ros@abv.bg>
2018-01-28 17:02:41 +0200

    -

---
+++ CaloriesCounter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CaloriesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfIngredients = int.Parse(Console.ReadLine());
+            int calories = 0;
+
+            for (int i = 0; i < numOfIngredients; i++)
+            {
+                string ingredient = Console.ReadLine().ToLower();
+
+                switch (ingredient)
+                {
+                    case "cheese": calories += 500;
+                        break;
+                    case "tomato sauce": calories += 150;
+                        break;
+                    case "salami": calories += 600;
+                        break;
+                    case "pepper": calories += 50;
+                        break;
+                    default: break;
+                }
+            }
+
+            Console.WriteLine($"Total calories: {calories}");
+        }
+    }
+}
