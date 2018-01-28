10d85d69e5de29cce474e40e5e84fabe7d3653a8
RositcaMadzharova <val.ros@abv.bg>
2018-01-28 15:53:58 +0200

    -

---
+++ IntervalOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Interval_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            for (int i = Math.Min(firstNumber, secondNumber); i <= Math.Max(firstNumber, secondNumber); i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
