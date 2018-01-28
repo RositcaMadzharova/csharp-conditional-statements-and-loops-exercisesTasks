95584cca6ea004984e20b35e1c1a37c81215b028
RositcaMadzharova <val.ros@abv.bg>
2018-01-28 16:29:37 +0200

    -

---
+++ CakeIngredients.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CakeIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingrediend = Console.ReadLine();
+            int count = 0;
+
+            while (ingrediend != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {ingrediend}.");
+                count++;
+                ingrediend = Console.ReadLine();
+            }
+            Console.WriteLine($"Preparing cake with {count} ingredients.");
+        }
+    }
+}
