1ff4ee9a666926d7602481f05223c6a2beb4e422
RositcaMadzharova <val.ros@abv.bg>
2018-01-28 12:17:20 +0200

    -

---
+++ RestaurantDiscount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace RestaurantDiscount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeoples = int.Parse(Console.ReadLine());
+            string package = Console.ReadLine();
+
+            string hallType = null;
+            int hallPrice = 0;
+
+            if (countOfPeoples <= 50)
+            {
+                hallType = "Small Hall";
+                hallPrice = 2500;
+            }
+            else if (countOfPeoples > 50 && countOfPeoples <= 100)
+            {
+                hallType = "Terrace";
+                hallPrice = 5000;
+            }
+            else if (countOfPeoples > 100 && countOfPeoples <= 120)
+            {
+                hallType = "Great Hall";
+                hallPrice = 7500;
+            }
+            int discount = 0;
+            int packagePrice = 0;
+
+            if (package == "Normal")
+            {
+                packagePrice = 500;
+                discount = 5;
+            }
+            else if (package == "Gold")
+            {
+                packagePrice = 750;
+                discount = 10;
+            }
+            else if (package == "Platinum")
+            {
+                packagePrice = 1000;
+                discount = 15;
+            }
+
+            if (hallPrice != 0)
+
+            {
+
+                int totalPrice = hallPrice + packagePrice;
+
+                double totalDiscount = totalPrice / 100.0 * discount;
+
+                double pricePerPerson = (totalPrice - totalDiscount) / countOfPeoples;
+
+                Console.WriteLine($"We can offer you the {hallType}");
+
+                Console.WriteLine($"The price per person is {pricePerPerson:f2}$");
+
+            }
+
+            else Console.WriteLine("We do not have an appropriate hall.");
+        }
+    }
+}
---
+++ WordInPlural.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Word_in_Plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string noun = Console.ReadLine();
+            int nounLength = noun.Length;
+            string plural;
+
+            if (noun.EndsWith("y"))
+            {
+                plural = noun.Remove(nounLength - 1);
+                plural = plural + "ies";
+            }
+            else if (noun.EndsWith("o") || noun.EndsWith("ch") || noun.EndsWith("s") 
+                  || noun.EndsWith("sh") || noun.EndsWith("x") || noun.EndsWith("z"))
+            {
+                plural = noun + "es";
+            }
+            else
+
+            {
+                plural = noun + "s";
+            }
+            Console.WriteLine(plural);
+        }
+    }
+}
