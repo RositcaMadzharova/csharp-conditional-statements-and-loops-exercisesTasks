9f131f978d1d0cc55f456ddae70e60e023a64e6a
RositcaMadzharova <val.ros@abv.bg>
2018-02-03 17:24:55 +0200

    *

---
+++ Magic Letter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Magic_Letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+
+            for (int i = first; i <= second; i++)
+            {
+                for (int j = first; j <= second; j++)
+                {
+                    for (int k = first; k <= second; k++)
+                    {
+                        if (i != third && j != third && k != third)
+                        {
+                            Console.WriteLine($"{i}{j}{k} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
