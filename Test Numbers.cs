1035503d157ec5d8cf83a102b116be75e3ea5539
RositcaMadzharova <val.ros@abv.bg>
2018-02-03 14:00:18 +0200

    -
    Write a program, which finds all the possible combinations between two numbers – N and M.
    The first digit decreases from N to 1, and the second digit increases from 1 to M. The two digits form a number. Multiply the two digits, then multiply their product by 3. Add the result to the total sum.
    You will also be given a third number, which will be the maximum boundary of the sum. If the sum is equal or greater than this number you should stop the program. See the examples for further information.

---
+++ Test Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int maxSum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int counter = 0;
+
+            for (int i = firstNum; i >= 1; i--)
+            {
+                for (int k = 0; k <= secondNum; k++)
+                {
+                    counter++;
+                    int curentNum = (i * k) * 3;
+                    sum += curentNum;
+
+                    if (sum >= maxSum)
+                    {
+                        Console.WriteLine($"{counter}combinations");
+                        Console.WriteLine($"Sum: {sum} >= {maxSum}");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{counter}combinations");
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
